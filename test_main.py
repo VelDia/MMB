4ff91f01f270e9459b34a9d1b7a44773ee58c21b
VelDia <sonechkodiana02@gmail.com>
2024-07-19 14:06:01 -0400

    Revert "Revert "Improving test_main""
    This reverts commit c3ef9c7acb6e34b3f4b89b17dd5ec91ed5ec6fc5.
    
    This revert did not solve the issue, returning progressed script

--- test_main.py
+++ test_main.py
@@ -1,48 +1,55 @@
-from algorithm1 import convert_to_binary_mask, morph_operations
+from algorithm1 import calc_difference_mask, remove_false_alarms, convert_to_binary_mask, morph_operations
 from algorithm2 import construct_data_matrix, estimate_rank, fRMC
 
 import cv2
 import os
 
-background_images_folder = '/home/diana/output_background_images'
+# background_images_folder = '/home/diana/output_background_images'
 
-main_path = '/home/diana/tracking/mot/'
+dataset_path = 'mot/'
 
 dict_folder = {
-    # 'car' : os.path.join(main_path, 'car'),
-    # 'plane' : os.path.join(main_path, 'plane'),
-    # 'ship' : os.path.join(main_path, 'ship'),
-    'train' : os.path.join(main_path, 'train')
+    # 'car' : os.path.join(dataset_path, 'car'),
+    # 'plane' : os.path.join(dataset_path, 'plane'),
+    # 'ship' : os.path.join(dataset_path, 'ship'),
+    'train' : os.path.join(dataset_path, 'train')
 }
 
 for name, name_path in dict_folder.items():
     # print(name_path)
     # print(os.listdir(name_path))
-    video_folder = [os.path.join((os.path.join(name_path, path)), 'img') for path in sorted(os.listdir(name_path))]
-
+    video_folder = [os.path.join((os.path.join(name_path, path)), 'img') for path in sorted(os.listdir(name_path)) if path != '.DS_Store']
     for video in video_folder:
-
-    # for i in range(1, m-2):
-        # im_path_list = focus_list[i:i+3]
-        # print(im_path_list)
-        # diff, mask = calc_difference_mask(im_path_list)
-        # masks.append(mask)
-        # image3 = cv2.imread(im_path_list[2], cv2.IMREAD_COLOR)
-        # save_path = os.path.join(opath_im, 'sp'+ str(i) +  '.jpg')
-        # mask = morph_operations(mask)
-        # result_image, rois = remove_false_alarms(mask, image3, save_path)
-        # roiss.append(rois)
-        # resulted_imgs.append(result_image) 
-        
-     
         images_list = sorted(os.listdir(video))
-        images_path = [os.path.join(video, image) for image in images_list]
+        print(images_list)
+        images_path = [os.path.join(video, image) for image in images_list if image != '.DS_Store']
         frames = [cv2.imread(image) for image in images_path]
-        print(len(frames))
+        m = len(frames)
+        print(m)
+
+        masks = []
+        roiss = []
+        resulted_imgs = []
+
+        opath_im = '/output_rois/' + name + '/'
+        for i in range(1, m-2):
+            im_path_list = images_path[i:i+3]
+            print(im_path_list)
+            diff, mask = calc_difference_mask(im_path_list)
+            masks.append(mask)
+            image3 = cv2.imread(im_path_list[2], cv2.IMREAD_COLOR)
+            save_path = os.path.join(opath_im, images_list[i], 'roi'+ str(i) +  '.jpg')
+
+            os.makedirs(save_path, exist_ok=True)
+            mask = morph_operations(mask)
+            result_image, rois = remove_false_alarms(mask, image3, save_path)
+            roiss.append(rois)
+            resulted_imgs.append(result_image) 
+        
         V = construct_data_matrix(frames)
         rank = estimate_rank(V)
         B = fRMC(V, rank)
-        # background_images = [B[:, i].reshape(frames[0].shape) for i in range(B.shape[1])]
+        background_images = [B[:, i].reshape(frames[0].shape) for i in range(B.shape[1])]
         # print(background_images)
         # foreground_images = []
         # for i in len(background_images):
@@ -52,7 +59,7 @@ for name, name_path in dict_folder.items():
         # binary_images = [convert_to_binary_mask(foreground) for foreground in foreground_images]
         # morphed_images = [morph_operations(binary_image) for binary_image in binary_images]
 
-        output_folder = os.path.join(main_path, 'output_background_images')
+        output_folder = os.path.join(dataset_path, 'output_background_images')
         os.makedirs(output_folder, exist_ok=True)
         for j in range(B.shape[1]):
             background = B[:, j].reshape(frames[0].shape)
