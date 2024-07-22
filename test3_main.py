260afeb3474a81276078332f2b862ba03033e890
VelDia <sonechkodiana02@gmail.com>
2024-07-22 17:29:43 -0400

    added metrics calculation output; small changes to gitignore

--- test3_main.py
+++ test3_main.py
@@ -7,6 +7,7 @@ from algorithm2 import saving_roi_from_mask
 # list of paths
 # opath_im = '/Users/diana/Desktop/tracking/MMB/output_rois'
 # opath_im = '/home/diana/MMB/output_rois'
+opath_im = '/home/diana/MMB/output_rois_limited'
 
 # Perform element-wise multiplication of masks
 mask_alg1_path = os.path.join(opath_im, 'masks_alg1')
@@ -41,14 +42,12 @@ for mask in masks_ult:
 
 # Example data
 gt_path = 'mot/car/001/gt/gt.txt'
-import csv
-
 
 # Read data from files
 # algorithm1_predictions = read_csv_file('algorithm1_predictions.txt')
 # algorithm2_predictions = read_csv_file('algorithm2_predictions.txt')
-ground_truths = read_csv_file('ground_truth.txt')
-
+ground_truths = read_csv_file(gt_path)
+print(ground_truths)
 algorithm1_predictions = roi1
 algorithm2_predictions = roi2
 
@@ -59,6 +58,10 @@ tp_algorithm2 = calculate_true_positives(algorithm2_predictions, ground_truths, 
 print(f"True Positives for Algorithm 1: {tp_algorithm1}")
 print(f"True Positives for Algorithm 2: {tp_algorithm2}")
 
+precision1, recall1, f1_score1 = calculate_metrics(roi1, ground_truths, 0.01)
+precision2, recall2, f1_score2 = calculate_metrics(roi2, ground_truths, 0.01)
+print("Alg 1: ", precision1, recall1, f1_score1)
+print("Alg 2: ", precision2, recall2, f1_score2)
 # # Algorithm 3 (Motion Trajectory-based False Alarm Filter)
 # # Parameters
 # pipeline_length = 5
