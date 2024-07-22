792a2ae2a00b5a55a1cf23cf2283fe7894551fec
VelDia <sonechkodiana02@gmail.com>
2024-07-22 13:33:17 -0400

    added code to calculate true positives

--- test3_main.py
+++ test3_main.py
@@ -1,11 +1,12 @@
 import cv2
 import os
 import numpy as np
-from part3 import is_true_positive 
+from part3 import calculate_true_positives, calculate_metrics, read_csv_file
 from algorithm2 import saving_roi_from_mask
 
 # list of paths
-opath_im = '/home/diana/MMB/output_rois'
+# opath_im = '/Users/diana/Desktop/tracking/MMB/output_rois'
+# opath_im = '/home/diana/MMB/output_rois'
 
 # Perform element-wise multiplication of masks
 mask_alg1_path = os.path.join(opath_im, 'masks_alg1')
@@ -26,19 +27,50 @@ for i in range(length-1):
     res_bound = np.where(res > 0, 255, 0)
     masks_ult = np.append(masks_ult, res_bound)
     cv2.imwrite(os.path.join(mask_ult_path, f'mask_ult{i}.png'), res_bound)
+roi1 = []
+for mask in masks:
+    roi1.append(saving_roi_from_mask(mask))
 
-# Algorithm 3 (Motion Trajectory-based False Alarm Filter)
-# Parameters
-pipeline_length = 5
-pipeline_size = (7, 7)
-detection_threshold = 3
-frame_skip_threshold = 4
-distance_threshold = 7
+roi2 = []
+for mask in masks2:
+    roi2.append(saving_roi_from_mask(mask))
 
-# for i in range(0, len(frames), )
-# for i in range(pipeline_length):
-#     current_frame = frames[i]
-rois1 = []
-# threshold = 0.5  # IoU threshold
-# result = is_true_positive(predicted_box, ground_truth_box, threshold)
-# print("Is True Positive:", result)
+roi3 = []
+for mask in masks_ult:
+    roi3.append(saving_roi_from_mask(mask))
+
+# Example data
+gt_path = 'mot/car/001/gt/gt.txt'
+import csv
+
+
+# Read data from files
+# algorithm1_predictions = read_csv_file('algorithm1_predictions.txt')
+# algorithm2_predictions = read_csv_file('algorithm2_predictions.txt')
+ground_truths = read_csv_file('ground_truth.txt')
+
+algorithm1_predictions = roi1
+algorithm2_predictions = roi2
+
+# Calculate TP for each algorithm
+tp_algorithm1 = calculate_true_positives(algorithm1_predictions, ground_truths, threshold=0.5)
+tp_algorithm2 = calculate_true_positives(algorithm2_predictions, ground_truths, threshold=0.5)
+
+print(f"True Positives for Algorithm 1: {tp_algorithm1}")
+print(f"True Positives for Algorithm 2: {tp_algorithm2}")
+
+# # Algorithm 3 (Motion Trajectory-based False Alarm Filter)
+# # Parameters
+# pipeline_length = 5
+# pipeline_size = (7, 7)
+# detection_threshold = 3
+# frame_skip_threshold = 4
+# distance_threshold = 7
+
+# # for i in range(0, len(frames), )
+# # for i in range(pipeline_length):
+# #     current_frame = frames[i]
+# rois1 = []
+# # threshold = 0.5  # IoU threshold
+# # result = is_true_positive(predicted_box, ground_truth_box, threshold)
+# # print("Is True Positive:", result)
