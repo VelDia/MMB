d47aecc19a5c55189c253c7c96042929b1a9f3ed
VelDia <sonechkodiana02@gmail.com>
2024-08-27 11:56:00 -0400

    minor changes that allow to run but run into issues

--- convlstm.py
+++ convlstm.py
@@ -1,8 +1,9 @@
 # import torch
 import tensorflow
 import ultralytics
-
-from tensorflow.keras.models import load_model
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
 
 # Load the pretrained YOLO model
 yolo_model = load_model('yolo_model.h5')
@@ -11,9 +12,10 @@ yolo_model = load_model('yolo_model.h5')
 for layer in yolo_model.layers:
     layer.trainable = False
 
-from tensorflow.keras.layers import ConvLSTM2D, BatchNormalization, Conv2D, Input
-from tensorflow.keras.models import Model
-
+from keras.layers import ConvLSTM2D, BatchNormalization, Conv2D, Input
+from keras.models import Model
+sequence_length = 3
+img_size = 1454
 # Create a new input layer to handle sequences
 sequence_input = Input(shape=(sequence_length,) + img_size)
 
@@ -25,7 +27,8 @@ x = ConvLSTM2D(filters=64, kernel_size=(3, 3), padding='same', return_sequences=
 x = BatchNormalization()(x)
 x = ConvLSTM2D(filters=64, kernel_size=(3, 3), padding='same', return_sequences=True)(x)
 x = BatchNormalization()(x)
-
+num_anchors = 9
+num_classes = 4
 # Add detection head
 output = Conv2D(filters=num_anchors*(num_classes+5), kernel_size=(1, 1), padding='same')(x)
 
@@ -37,5 +40,5 @@ model = Model(inputs=sequence_input, outputs=output)
 model.compile(optimizer='adam', loss='your_loss_function', metrics=['accuracy'])
 
 # Train the model on your custom dataset
-model.fit(train_dataset, epochs=10, validation_data=val_dataset)
+model.fit(data="/home/diana/MMB/viso.yaml", epochs=10)
 
